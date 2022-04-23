ccff234355a846725c9c3fede278e5061a73fada
cyber-punk20 <62537341+cyber-punk20@users.noreply.github.com>
2022-04-23 15:47:34 -0500

    first commit

---
+++ test/sample-test.js
@@ -0,0 +1,45 @@
+require('web3')
+describe("NFTMarket", function() {
+  it("Should create and execute market sales", async function() {
+    const Market = await ethers.getContractFactory("NFTMarket")
+    const market = await Market.deploy()
+    await market.deployed()
+    const marketAddress = market.address
+
+    const NFT = await ethers.getContractFactory("NFT")
+    const nft = await NFT.deploy(marketAddress)
+    await nft.deployed()
+    const nftContractAddress = nft.address
+
+    let listingPrice = await market.getListingPrice()
+    listingPrice = listingPrice.toString()
+
+    const auctionPrice = ethers.utils.parseUnits('1', 'ether')
+
+    await nft.createToken("https://www.mytokenlocation.com")
+    await nft.createToken("https://www.mytokenlocation2.com")
+
+    hextag = web3.utils.asciiToHex('Storm');
+    tag = web3.utils.hexToBytes(hextag)
+    await market.createMarketItem(nftContractAddress, 1, auctionPrice, { value: listingPrice }, [tag])
+    await market.createMarketItem(nftContractAddress, 2, auctionPrice, { value: listingPrice }, [tag])
+    
+    const [_, buyerAddress] = await ethers.getSigners()
+
+    await market.connect(buyerAddress).createMarketSale(nftContractAddress, 1, { value: auctionPrice})
+
+    items = await market.fetchMarketItems()
+    items = await Promise.all(items.map(async i => {
+      const tokenUri = await nft.tokenURI(i.tokenId)
+      let item = {
+        price: i.price.toString(),
+        tokenId: i.tokenId.toString(),
+        seller: i.seller,
+        owner: i.owner,
+        tokenUri
+      }
+      return item
+    }))
+    console.log('items: ', items)
+  })
+})
