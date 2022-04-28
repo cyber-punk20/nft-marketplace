3174fad56da6cba517150af28d96d9c2308a1595
harrisonberrier <harrison_berrier@yahoo.com>
2022-04-28 12:38:25 -0500

    Navbar styling
    Added images and styled navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   content: [
     "./pages/*.{js,jsx,ts,tsx}",
   ],
+  darkMode: 'media', // false or 'media' or 'class'
   theme: {
+    extend: {},
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
